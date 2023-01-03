b68196c5df0d0efa3d2569ec8672606142c71f10
BagretsovDm <bagretsovdmitry@gmail.com>
2023-01-03 12:34:40 +0200

    добавление удаления элемента при свайпе влево

--- app/src/main/java/otus/gpb/recyclerview/MainActivity.kt
+++ app/src/main/java/otus/gpb/recyclerview/MainActivity.kt
@@ -2,6 +2,8 @@ package otus.gpb.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.divider.MaterialDividerItemDecoration
 import com.google.android.material.divider.MaterialDividerItemDecoration.VERTICAL
 import otus.gpb.recyclerview.databinding.ActivityMainBinding
@@ -29,13 +31,35 @@ class MainActivity : AppCompatActivity() {
     private fun setupView() {
         val decorator = MaterialDividerItemDecoration(this, VERTICAL).apply {
             dividerColor = resources.getColor(R.color.dividerColor, theme)
-            dividerInsetStart = 240
+            dividerInsetStart =
+                this@MainActivity.resources.getDimensionPixelSize(R.dimen.divider_padding_start)
             isLastItemDecorated = false
         }
+
+        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.Callback() {
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int {
+                return makeMovementFlags(0, ItemTouchHelper.LEFT)
+            }
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ) = false
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                adapter.removeItem(viewHolder.adapterPosition)
+            }
+        })
 
         with(binding) {
             recyclerView.adapter = adapter
             recyclerView.addItemDecoration(decorator)
+
+            itemTouchHelper.attachToRecyclerView(recyclerView)
         }
     }
 }
