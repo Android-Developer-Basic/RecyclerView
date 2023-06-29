e10f0830ec6386b90b48fe0227ef0685af81f935
LeonZhuk <lionzhuk@gmail.com>
2023-06-29 13:55:12 +0300

    DZ

---
+++ app/src/main/java/otus/gpb/recyclerview/model/MainModel.kt
@@ -0,0 +1,17 @@
+package otus.gpb.recyclerview.model
+
+
+data class MainModel(
+    val id: Int,
+    val avatar: Int,
+    val preview: Int?,
+    val nick: String,
+    val status: String,
+    val last_massage: String,
+    val time: String,
+    val count: Int = 0,
+    val check: Int?,
+    val isSCAM: Boolean = false,
+    val isMute: Boolean = false,
+    val isVerified: Boolean = false
+)
