aaff0216a46dacc7b591d18e6082fbf29aa327cf
Grave1ine <vladislav_barinov@bk.ru>
2023-07-10 16:07:26 +0300

    recyclerView

---
+++ app/src/main/java/otus/gpb/recyclerview/Adapter.kt
@@ -0,0 +1,39 @@
+package otus.gpb.recyclerview
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import otus.gpb.recyclerview.model.movieModel
+
+class Adapter: RecyclerView.Adapter<ViewHolder>() {    //создает viewHolder, переписывает его
+
+    private var item = mutableListOf<movieModel>()     //изменяемый списко типа movieModel
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setItems(newItems: List<movieModel>) {
+        item = newItems.toMutableList()                //toMutableList создает изменяемый список из newItems (Результат выполнения - новая коллекция, но с элементами из исходной коллекции)???
+        notifyDataSetChanged()                         //указывает адаптеру, что полученные ранее данные изменились и следует перерисовать список на экране.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {                 //просто запомнить, всегда одинаковое
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false) //создается ViewHolder(разметка)
+        return ViewHolder(view)                                                                     //возвращаем вью холдер передав в него разметку?
+    }
+
+    override fun getItemCount(): Int {                                                              //возвращаем размер списка элементов вьюхолдера
+        return item.size
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {                              //передаем данные во вьюхолдер
+        holder.bind(item[position])
+    }
+
+    fun removeItem(position: Int) {
+        val newItems = item.toMutableList()
+        newItems.removeAt(position)          //удаляем позицию по полученному номеру
+        item = newItems                      //переписываем
+        notifyItemRemoved(position)
+    }
+}
--- app/src/main/java/otus/gpb/recyclerview/MainActivity.kt
+++ app/src/main/java/otus/gpb/recyclerview/MainActivity.kt
@@ -1,12 +1,89 @@
 package otus.gpb.recyclerview
 
+import android.graphics.Canvas
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
+    private val adapter by lazy { Adapter() }         //создание адаптера
+    private val presenter by lazy { Presenter() }     //создание данных
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val context = this.applicationContext   //получаем контекст активити????
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)  //подгружаем разметку ресайклера
+        recyclerView.adapter = adapter                                    //дрбавить адаптер???
+        adapter.setItems(presenter.getItems())
+
+        val manager = LinearLayoutManager(this) //LinearLayoutManager - упорядочивает элементы в виде обычного вертикального или горизонтального списка указывать обязательно или тут или в хмл
+        recyclerView.layoutManager = manager
+
+        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.Callback(){          //скип влево удалить
+
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+            ): Int {
+                return makeMovementFlags(ItemTouchHelper.DOWN, ItemTouchHelper.LEFT)
+            }
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder,
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                adapter.removeItem(position)
+            }
+
+            override fun onChildDraw(                           //  картинка присвайпе, наверно всегда одинаковая
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                val itemView = viewHolder.itemView
+                val itemHeight = itemView.bottom - itemView.top
+
+                val icon = ContextCompat.getDrawable(context, R.drawable.dond_lod)
+                val background = ColorDrawable()
+                background.color = ContextCompat.getColor(context, R.color.main)
+
+                val iconMargin = (itemHeight - icon!!.intrinsicHeight) / 2    //прогресс свайпа???
+                val iconTop = itemView.top + iconMargin
+                val iconBottom = iconTop + icon.intrinsicHeight
+                val iconRight = itemView.right - iconMargin
+                val iconLeft = iconRight - icon.intrinsicWidth
+
+                icon.setBounds(iconLeft, iconTop, iconRight, iconBottom)
+                background.setBounds(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+
+                background.draw(c)    //рисуем иконку
+                icon.draw(c)
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)  //рисуем по дефолту?
+            }
+
+
+        })
+        itemTouchHelper.attachToRecyclerView(recyclerView)
     }
 }
