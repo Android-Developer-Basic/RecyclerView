78ea3f9b3ba75dc6a650321275789a0e7415f29b
Alexey Kozyrev <tcsid4@gmail.com>
2023-11-29 12:58:54 +0300

    подготовлена верстка

--- app/src/main/java/otus/gpb/recyclerview/model/Chat.kt
+++ app/src/main/java/otus/gpb/recyclerview/model/Chat.kt
@@ -3,6 +3,7 @@ package otus.gpb.recyclerview.model
 data class Chat(
     val id: ULong,
     val nameUserInChat: String,
+    val urlAvatar: String, //ссылка на аватар, либо с интернета либо с файлов
     val statusNotification: Boolean, //без уведомлений
     val previewText: String,
     val timeLastMessage: String,
