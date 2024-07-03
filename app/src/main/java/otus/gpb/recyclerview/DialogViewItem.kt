c3510ddcb320ec1b5d6ee6d3db0d8252e25b1474
Nikolay (nazarovNV) <nikolaynazarovvv216@gmail.com>
2024-07-03 19:23:30 +0300

    Сделал половину первого пункта

---
+++ app/src/main/java/otus/gpb/recyclerview/DialogViewItem.kt
@@ -0,0 +1,12 @@
+package otus.gpb.recyclerview
+
+data class DialogViewItem(
+    val id: Int,
+    val name: String,
+    val title: String,
+    val message: String,
+    val time: String,
+    val image: Int
+)
+{
+}
