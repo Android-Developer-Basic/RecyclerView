5b517c3fc7955aba36608c0c03c281809aa193fd
nikolay <nazarov.nikolay15@wb.ru>
2024-07-05 23:46:13 +0300

    Не работает анимация свайпа

--- app/src/main/java/otus/gpb/recyclerview/SwipeToDeleteCallback.kt
+++ app/src/main/java/otus/gpb/recyclerview/SwipeToDeleteCallback.kt
@@ -1,7 +1,11 @@
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import otus.gpb.recyclerview.ChatAdapter
+import otus.gpb.recyclerview.R
 
 open class SwipeToDeleteCallback(private val adapter: ChatAdapter) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
 
@@ -15,6 +19,26 @@ open class SwipeToDeleteCallback(private val adapter: ChatAdapter) : ItemTouchHe
     }
 
     override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+        val itemView = viewHolder.itemView
+        val background = ColorDrawable(Color.RED) // Цвет фона при свайпе
+
+        // Рисуем фон
+        background.setBounds(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+        background.draw(c)
+
+        // Рисуем иконку удаления
+        val deleteIcon = ContextCompat.getDrawable(itemView.context, R.drawable.icon_not_found)
+        val iconMargin = (itemView.height - deleteIcon?.intrinsicHeight!!) / 2
+        val iconTop = itemView.top + (itemView.height - deleteIcon.intrinsicHeight) / 2
+        val iconBottom = iconTop + deleteIcon.intrinsicHeight
+
+        if (dX < 0) {
+            val iconLeft = itemView.right - iconMargin - deleteIcon.intrinsicWidth
+            val iconRight = itemView.right - iconMargin
+            deleteIcon.setBounds(iconLeft, iconTop, iconRight, iconBottom)
+            deleteIcon.draw(c)
+        }
+
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
     }
 }
