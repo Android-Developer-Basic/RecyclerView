e3a7275a6f46c36734e912e50c5c34c9a6764705
nikolay <nazarov.nikolay15@wb.ru>
2024-07-06 01:17:26 +0300

    Поменял иконку

--- app/src/main/java/otus/gpb/recyclerview/MainActivity.kt
+++ app/src/main/java/otus/gpb/recyclerview/MainActivity.kt
@@ -31,19 +31,20 @@ class MainActivity : AppCompatActivity() {
         val swipeHandler = object : SwipeToDeleteCallback(adapter) {
             override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
                 val itemView = viewHolder.itemView
-                val background = ColorDrawable(Color.RED) // Цвет фона при свайпе
+                val swipeColor = ContextCompat.getColor(itemView.context, R.color.swipe_color)
+                val background = ColorDrawable(swipeColor) // Цвет фона при свайпе
 
                 // Рисуем фон
                 background.setBounds(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
                 background.draw(c)
 
                 // Рисуем иконку удаления
-                val deleteIcon = ContextCompat.getDrawable(itemView.context, R.drawable.icon_not_found)
+                val deleteIcon = ContextCompat.getDrawable(itemView.context, R.drawable.package_down)
                 val iconMargin = (itemView.height - deleteIcon?.intrinsicHeight!!) / 2
                 val iconTop = itemView.top + (itemView.height - deleteIcon.intrinsicHeight) / 2
                 val iconBottom = iconTop + deleteIcon.intrinsicHeight
 
-                if (dX < 0) {
+                if (dX < -50) {
                     val iconLeft = itemView.right - iconMargin - deleteIcon.intrinsicWidth
                     val iconRight = itemView.right - iconMargin
                     deleteIcon.setBounds(iconLeft, iconTop, iconRight, iconBottom)
