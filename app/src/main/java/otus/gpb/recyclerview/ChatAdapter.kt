c45106aa80f28959871042e51ed15ce2215b4f93
nikolay <nazarov.nikolay15@wb.ru>
2024-07-12 22:37:34 +0300

    Добавил иконку мута

--- app/src/main/java/otus/gpb/recyclerview/ChatAdapter.kt
+++ app/src/main/java/otus/gpb/recyclerview/ChatAdapter.kt
@@ -10,12 +10,14 @@ import androidx.recyclerview.widget.RecyclerView
 class ChatAdapter(private val items: MutableList<ChatItem>) : RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
     class ChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val name: TextView by lazy { itemView.findViewById(R.id.nameTV) }
+        private val isMuted: ImageView by lazy { itemView.findViewById(R.id.MutedIcon) }
         private val title: TextView by lazy { itemView.findViewById(R.id.titleTV) }
         private val message: TextView by lazy { itemView.findViewById(R.id.messageTV) }
         private val time: TextView by lazy { itemView.findViewById(R.id.timeTV) }
         private val image: ImageView by lazy { itemView.findViewById(R.id.imageAvatar) }
         fun bind(item: ChatItem){
             name.text = item.name
+            if (item.isMuted) isMuted.setImageResource(R.drawable.volume_off)
             title.text = item.title
             message.text = item.message
             time.text = item.time
